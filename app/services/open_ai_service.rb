2bf601e10e18213ced98b5523e57c763b1b6cde1
JeanRemi Charrier <jeanremi.char@gmail.com>
2024-11-26 18:00:52 +0100

    openai ça avance bien

--- app/services/open_ai_service.rb
+++ app/services/open_ai_service.rb
@@ -16,22 +16,22 @@ class OpenAiService
     prompt = build_prompt
     chatgpt_response = client.chat(parameters: {
       model: "gpt-4o-mini",
-      messages: [{ role: "user", content: prompt}]
+      messages: [{ role: "user", content: prompt}],
       temperature: 0.6
     })
-    steps = chatgpt_response[]
-
+    return chatgpt_response["choices"][0]["message"]["content"]
   end
 
   def build_prompt
     <<~PROMPT
-      Generate a round-trip itinerary between #{@sloopy.origin} and #{@sloopy.destination} for a trip from #{@sloopy.start_date}, to #{@sloopy.return_date}, with a maximum transport budget of #{@sloopy.budget}.
+      Generate a round-trip itinerary between #{@sloopy.origin} and #{@sloopy.destination} for a trip from #{@sloopy.departure_date}, to #{@sloopy.return_date}, with a maximum transport budget of #{@sloopy.budget}.
       The itinerary should include:
-      Outbound: Several stops where I can stay a few days (at least 2 stops between #{@sloopy.origin} and #{sloopy.destination}).
-      In #{sloopy.destination}: A stay of #{sloopy.duration} full days.
+      Outbound: Several stops where I can stay a few days (at least 2 stops between #{@sloopy.origin} and #{@sloopy.destination}).
+      In #{@sloopy.destination}: A stay of #{@sloopy.duration} full days.
       Return: A different itinerary from the outbound, with at least 1 or 2 new stops where I can stay for one or more nights.
-      Return to #{sloopy.origin} on #{sloopy.return_date}.
+      Return to #{@sloopy.origin} on #{@sloopy.return_date}.
       Provide the preferred modes of transport (car, bike, bus, train, boat, carpooling) to stay within the budget, and a summary of the costs for each step.
+      Without any of your own answer like 'Here is a round-trip itinerary'.
       PROMPT
   end
 end
