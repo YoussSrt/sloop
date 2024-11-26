cff04f86da6ee803607f13538504d84334e47d0d
JeanRemi Charrier <jeanremi.char@gmail.com>
2024-11-26 17:48:35 +0100

    pundit ca degage + controller sloopies bien avancé + openai

--- app/controllers/sloopies_controller.rb
+++ app/controllers/sloopies_controller.rb
@@ -1,21 +1,18 @@
 class SloopiesController < ApplicationController
   def index
-    @sloopies = policy_scope(Sloopy)
+    @sloopies = Sloopy.all
   end
 
   def show
-    # @sloopy = Sloopy.find_by(id: params[:id])
-    authorize @sloopy
+    @sloopy = Sloopy.find_by(id: params[:id])
   end
 
   def new
     @sloopy = Sloopy.new
-    authorize @sloopy
   end
 
   def create
     @sloopy = Sloopy.new(sloopy_params)
     @sloopy.user = current_user
-    authorize @sloopy
   end
 end
