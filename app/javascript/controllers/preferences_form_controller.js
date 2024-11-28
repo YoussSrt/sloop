e55fd226225e6d570d081c8463235103924e7376
daisy oliani <daisy.oliani@gmail.com>
2024-11-28 17:55:00 +0100

    formulaire

--- app/javascript/controllers/preferences_form_controller.js
+++ app/javascript/controllers/preferences_form_controller.js
@@ -1,1 +1,20 @@
-// 
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="preferences-form"
+export default class extends Controller {
+  connect() {
+    // console.log(this.element)
+    this.inputs = this.element.querySelectorAll('.form-check-input')
+  }
+
+  check(event) {
+    const list = Array.from(this.inputs)
+    const filteredList = list.filter((element) => {
+      return element.checked === true
+    });
+    if (filteredList.length >= 4) {
+      event.currentTarget.checked = false
+      alert('You already did 3 choices for this question')
+    }
+  }
+}
