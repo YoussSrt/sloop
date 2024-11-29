3b5155230fa7bdca5062f129ae0ee3af6a275987
Youssef Saroute <youssef.saroute77@gmail.com>
2024-11-29 11:36:08 +0100

    map stylé avec pin points

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "profile", to: "pages#profile"
-  post "preferences", to: "users#update_preferences", as: :preferences 
-  
+  post "preferences", to: "users#update_preferences", as: :preferences
+
 
   # Defines the root path route ("/")
   # root "posts#index"
   resources :sloopies
 
-  resources :chatrooms, only: [:show, :create] do
+  resources :chatrooms, only: [:index, :show, :create] do
     resources :messages, only: [:create]
   end
 end
