361050500865aadf57d7f7ca44597b5974f40175
Youssef Saroute <youssef.saroute77@gmail.com>
2024-12-03 22:52:55 +0100

    revue de toute la messagerie

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,7 +3,8 @@ class ChatroomsController < ApplicationController
   before_action :set_chatroom, only: [:show]
 
   def index
-    first_chatroom = Chatroom.where("first_user_id = :id OR second_user_id = :id", id: current_user.id).first
+    @chatrooms = Chatroom.where("first_user_id = :id OR second_user_id = :id", id: current_user.id)
+    first_chatroom = @chatrooms.first
     redirect_to first_chatroom ? chatroom_path(first_chatroom) : root_path
   end
   
@@ -11,20 +12,25 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
     @users = User.where.not(id: current_user.id)
-    @chatrooms = Chatroom.where("first_user_id = :user_id OR second_user_id = :user_id", user_id: current_user.id)
+    @chatrooms = Chatroom.where("first_user_id = :id OR second_user_id = :id", id: current_user.id)
+                        .left_joins(:messages)
+                        .group("chatrooms.id")
+                        .select("chatrooms.*, COALESCE(MAX(messages.created_at), chatrooms.created_at) as last_message_time")
+                        .order("last_message_time DESC")
   end
 
 
 
   def create
+    first_user = current_user
     second_user = User.find(params[:second_user_id])
-    @chatroom = Chatroom.new(first_user: current_user, second_user: second_user)
 
-    if @chatroom.save
-      redirect_to chatroom_path(@chatroom)
-    else
-      redirect_to root_path, alert: "Impossible de créer la conversation"
-    end
+    @chatroom = Chatroom.between_users(first_user, second_user).first_or_create!(
+      first_user: first_user,
+      second_user: second_user
+    )
+
+    redirect_to chatroom_path(@chatroom)
   end
 
   private
