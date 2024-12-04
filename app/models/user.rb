0f71e645d8dafa4717d8c58d2b89e05e748c4fa1
JeanRemi Charrier <jeanremi.char@gmail.com>
2024-12-04 11:38:48 +0100

    ajout avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id', dependent: :destroy
   has_many :chatrooms_as_first_user, class_name: 'Chatroom', foreign_key: 'first_user_id', dependent: :destroy
   has_many :chatrooms_as_second_user, class_name: 'Chatroom', foreign_key: 'second_user_id', dependent: :destroy
+  has_one_attached :avatar
 
   validates :first_name, presence: true
   validates :last_name, presence: true
